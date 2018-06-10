960fd3e26af23a54d3baa1ac23eece97fd18edbd
Shengmin <leongshengmin@gmail.com>
2018-06-10 23:03:34 +0800

    fixed merge conflict

--- app/src/main/java/com/eatlah/eatlah/models/User.java
+++ app/src/main/java/com/eatlah/eatlah/models/User.java
@@ -3,12 +3,8 @@ package com.eatlah.eatlah.models;
 public class User {
     private String _id;
     private String email;
-<<<<<<< HEAD
     private String address;
-=======
     private String _hawkerId;
->>>>>>> 638864da9567ec2d3a8521328cf132c08c99c187
-
 
     public User(String _id, String email, String hawkerId) {
         this._id = _id;
@@ -39,13 +35,11 @@ public class User {
         this._id = _id;
     }
 
-<<<<<<< HEAD
     public void setAddress(String address) {
         this.address = address;
     }
-=======
+
     public String get_hawkerId() { return _hawkerId; }
 
     public void set_hawkerId(String _hawkerId) { this._hawkerId = _hawkerId; }
->>>>>>> 638864da9567ec2d3a8521328cf132c08c99c187
 }
