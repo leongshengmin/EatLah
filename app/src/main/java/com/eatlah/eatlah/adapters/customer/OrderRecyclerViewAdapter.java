f8808666b55bf09a5e8fca78ba584e06773eac73
zioul123 <zioul.rsmod123@gmail.com>
2018-06-27 12:57:51 +0800

    Make customer and hawker cardholders all aesthetic-er.

--- app/src/main/java/com/eatlah/eatlah/adapters/customer/OrderRecyclerViewAdapter.java
+++ app/src/main/java/com/eatlah/eatlah/adapters/customer/OrderRecyclerViewAdapter.java
@@ -1,18 +1,23 @@
 package com.eatlah.eatlah.adapters.customer;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.eatlah.eatlah.R;
 import com.eatlah.eatlah.activities.CustomerHomepage;
 import com.eatlah.eatlah.fragments.customer.OrderFragment.OnListFragmentInteractionListener;
 import com.eatlah.eatlah.listeners.OnSwipeTouchListener;
 import com.eatlah.eatlah.models.OrderItem;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.List;
 
@@ -70,6 +75,25 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
                 notifyItemRemoved(position);
             }
         });
+
+        String imagePath = orderItem.getImage_path();
+        if (imagePath != null && !imagePath.isEmpty()) {
+            FirebaseStorage
+                .getInstance()
+                .getReference("FoodItems")
+                .child(imagePath)
+                .getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Glide
+                            .with(holder.mImageView.getContext())
+                            .load(uri.toString())
+                            .into(holder.mImageView);
+                    }
+                });
+        }
+
     }
 
     private void calculatePrice(OrderItem orderItem, TextView priceView) {
@@ -89,6 +113,7 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
         private TextView mPrice;
         private TextView mDesc;
         private Button mCancelButton;
+        private ImageView mImageView;
 
         public ViewHolder(View view) {
             super(view);
@@ -97,6 +122,7 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
             mPrice = view.findViewById(R.id.price_textView);
             mDesc = view.findViewById(R.id.orderAddress_textView);
             mCancelButton = view.findViewById(R.id.viewOrders_button);
+            mImageView = view.findViewById(R.id.orderItem_image);
         }
 
     }
